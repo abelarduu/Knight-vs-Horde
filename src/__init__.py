874a176f31475ff2a74b39793127f5407c14b417
Abel Lucas <abelucas009@gmail.com>
2025-03-24 23:22:21 -0300

    Testando a Grama no chão

--- src/__init__.py
+++ src/__init__.py
@@ -1,13 +1,28 @@
-from ursina import Ursina, Entity, mouse, Sky
+from ursina import *
 from src.player import Player
 
 app = Ursina()
 mouse.visible = False
 Sky()
 
+ground = Entity(model="quad",
+                color= "#70c6a9",
+                scale=(650, 650),
+                rotation_x=90,
+                position=(0, -5, 0))
+                
+grass = Entity(model="quad",
+                texture="src/assets/grass.png",
+                position=(0, -2, 1),
+                scale=(15, 4, 0))
+
 # Declarando Entidades
 player = Player()
+
 goblin = Entity(model="quad",
                 texture="src/assets/goblin.png",
                 position=(0, -1, 2),
                 scale=(5, 5, 0))
+                
+                                
+
--- src/player.py
+++ src/player.py
@@ -1,4 +1,8 @@
-from ursina import Entity
+from ursina import Entity, camera
+
+CAMERA_POSITION = camera.position
+INITIAL_SWORD_POSITION = CAMERA_POSITION + (5, -1, 0)
+INITIAL_SHIELD_POSITION = CAMERA_POSITION + (-5, -2, 0)
 
 class Player:
     def __init__(self):
@@ -6,24 +10,24 @@ class Player:
         self.scores = 0
         self.is_attacking = False
         self.is_defending = False
-    
+
         self.sword = Entity(model="quad",
                        texture="src/assets/sword.png",
-                       position=(5, -1, 0),
+                       position= INITIAL_SWORD_POSITION,
                        scale=(3, 7, 0))
 
         self.shield = Entity(model="quad",
                         texture="src/assets/shield.png",
-                        position=(-5, -2, 0),
+                        position= INITIAL_SHIELD_POSITION,
                         scale=(6, 6, 0))
         
     def idle_sword(self):
         self.sword.animate_rotation((0, 0, 0), duration=0.2)
-        self.sword.animate_position((5, -1, 0), duration=0.2)
+        self.sword.animate_position(INITIAL_SWORD_POSITION, duration=0.2)
         
     def idle_shield(self):
         self.shield.animate_rotation((0, 0, 0), duration=0.2)
-        self.shield.animate_position((-5, -2, 0), duration=0.2)
+        self.shield.animate_position(INITIAL_SHIELD_POSITION, duration=0.2)
     
     def attack(self):
         self.sword.animate_rotation((-25, 25, -25), duration=0.2)
